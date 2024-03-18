376bab69ffd62b35c04ddcd5bc1b86347e40ea6e
ahndb <asi0000@nate.com>
2024-03-18 16:48:43 +0900

    240318 17:50

--- app/src/main/java/gradle/App.java
+++ app/src/main/java/gradle/App.java
@@ -3,11 +3,23 @@
  */
 package gradle;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+// @NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
 class Person {
 
+  private final String name;
+  private int age;
 }
 
 public class App {
@@ -17,5 +29,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        Person person = new Person("홍길동", 20);
+        person = new Person("고길동");
+
+        person = Person.builder().name("홍길동").age(20).build();
+
     }
 }
